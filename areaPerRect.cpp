abd3d66b0b6a528bb76f063e9cd462e2ac0e88e0
Kevin Csiffary <kevin.csiffary@gmail.com>
2022-09-15 22:02:25 +0000

    Created basicMath.cpp and areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 Kevin Csiffary All rights reserved.
+//
+// Created by: Kevin Csiffary
+// Date: Sep. 15, 2022
+// This program calculates the area and perimeter of a rectangle
+// With dimention 3cm * 7cm
+
+#include <iostream>
+
+int main() {
+    std::cout << "if a rectangle has the dimentions:" << std::endl;
+    std::cout << "4cm * 6cm\n" << std::endl;
+    std::cout << "Then its area should be:" << std::endl;
+    std::cout << (4 * 6) << "cm²\n" << std::endl;
+    std::cout << "and its perimeter should be:" << std::endl;
+    std::cout << (2 * (4 + 6)) << "cm" << std::endl;
+}
---
+++ basicMath.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2022 Kevin Csiffary All rights reserved.
+//
+// Created by: Kevin Csiffary
+// Date: Sep. 15, 2022
+// This program does basic math
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "5+4=" << (5 + 4) << std::endl;
+    std::cout << "16-14=" << (16 - 14) << std::endl;
+    std::cout << "34/3=" << (34 / 3) << std::endl;
+    std::cout << "4*23=" << (4 * 23) << std::endl;
+    std::cout << "pow(3, 3)=" << (pow(3, 3)) << std::endl;
+}
